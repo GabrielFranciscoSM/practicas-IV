a21084053528f150641edd21edf223238f27612e
José Antonio Carmona Molina <83080951+jacarmona364@users.noreply.github.com>
2025-11-06 13:32:42 +0100

    feat: Elimino término principal en los artículos, closes #15
    Decidimos eliminar el parámetro 'terminoPrincipal' del constructor y su validación debido a que no es necesario de cara a la aplicación de la lógica de negocio.

--- src/services/agrupadorarticulos.ts
+++ src/services/agrupadorarticulos.ts
@@ -2,18 +2,12 @@ import { Articulo } from "../model/Articulo";
 
 export class AgrupadorArticulos {
   public readonly articulos: Articulo[];
-  public readonly terminoPrincipal: string;
 
-  constructor(articulos: Articulo[], terminoPrincipal: string) {
+  constructor(articulos: Articulo[]) {
     if (!articulos || articulos.length === 0) {
       throw new Error("El agrupador debe contener al menos un artículo.");
     }
 
-    if (!terminoPrincipal.trim()) {
-      throw new Error("El término principal no puede estar vacío.");
-    }
-
     this.articulos = articulos;
-    this.terminoPrincipal = terminoPrincipal.trim();
   }
 }
