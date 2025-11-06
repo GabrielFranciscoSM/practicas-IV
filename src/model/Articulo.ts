dea2305d35cb900e161d1eebd8dd7182bd09e50f
José Antonio Carmona Molina <83080951+jacarmona364@users.noreply.github.com>
2025-11-06 01:30:59 +0100

    feat:  Cambio estructura Articulo de map a objeto con atributos Closes #12
    Se decide cambiar la forma de expresar el artículo, pues de la manera anterior no expresa correctamente sus características.

--- src/model/Articulo.ts
+++ src/model/Articulo.ts
@@ -1,12 +1,14 @@
 export class Articulo {
-  public readonly info: Map<string, Date>;
+  public readonly titulo: string;
+  public readonly fecha: Date;
   public readonly texto: string;
 
   constructor(titulo: string, fecha: Date, texto: string) {
     if (!titulo.trim()) throw new Error("El título no puede estar vacío.");
     if (!texto.trim()) throw new Error("El texto del artículo no puede estar vacío.");
 
-    this.info = new Map([[titulo.trim(), fecha]]);
+    this.titulo = titulo.trim();
+    this.fecha = fecha;
     this.texto = texto.trim();
   }
 }
