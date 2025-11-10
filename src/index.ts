55c88f6a75053d68ab2585443abbe2d4b7824251
José Antonio Carmona Molina <83080951+jacarmona364@users.noreply.github.com>
2025-11-10 03:26:23 +0100

    fix: Modificamos el nombre de agrupacionArticulos a coleccionOrdenada, closes #21
    Se actualiza conforme a la modificación del nombre de la clase coleccionOrdenada.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./model/Articulo";
 export * from "./model/Coleccion";
 export * from "./model/Grupo";
-export * from "./services/agrupadorarticulos";
+export * from "./services/coleccionordenada";
