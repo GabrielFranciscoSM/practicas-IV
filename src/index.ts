d1524dcb59738597a5b3daef40f86c4bd828fb45
José Antonio Carmona <e.jacarmona@go.ugr.es>
2025-11-25 20:00:29 +0100

    Reinicio el Objetivo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export * from './models/Articulo';
-export * from './models/Coleccion';
-export * from './models/Grupo';
 
-export * from './services/ColeccionOrdenada';
--- src/model/Grupo.ts
+++
@@ -1,5 +0,0 @@
-import { Coleccion } from "./coleccion";
-
-export class Grupo extends Coleccion {
-    tema: string;
-}
